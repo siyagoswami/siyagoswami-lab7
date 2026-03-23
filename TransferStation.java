42a4eaa606969ec9073899a3e3e54fbf38a45330
siyagoswami <siya.goswami7@gmail.com>
2026-03-23 16:57:50 -0400

    completed TransferStation class

--- TransferStation.java
+++ TransferStation.java
@@ -1,3 +1,34 @@
+import java.util.ArrayList; 
+
 public class TransferStation extends Station {
+
+    public ArrayList<Station> transfers; 
+
+    public TransferStation(String color, String stop) {
+        super(color, stop);
+        transfers = new ArrayList<Station>();
+    }
+
+    public void addTransferStationPrev(Station s) {
+        transfers.add(s); 
+        s.nextStation = this; 
+    }
+
+    public void addTransferStationNext(Station s) {
+        transfers.add(s); 
+        s.prevStation = this; 
+    }
+
+    public String toString() {
+        String result = "TRANSFERSTATION " + stop + ": " + color + " line, " + "in service: " + inService + 
+        ", previous station: " + (prevStation == null ? "none" : prevStation.stop) + ", next station: " + (nextStation == null ? "none" : nextStation.stop) +
+        "\n\tTransfers: \n"; 
+
+        for(Station s : transfers) {
+            result += "\t" + s.toString() + "\n";     
+        }
+
+        return result; 
+    }
     
 }
