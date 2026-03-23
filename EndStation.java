cd8051602b8f9cfc04e0fecde973facd157afddf
siyagoswami <siya.goswami7@gmail.com>
2026-03-23 15:55:35 -0400

    completed EndStation class w/ added makeEnd() method

--- EndStation.java
+++ EndStation.java
@@ -1,8 +1,20 @@
 public class EndStation extends Station {
-
-    nextStation = "none"; 
     
     public EndStation(String color, String stop) {
         super(color, stop);
+    }
+
+    public void makeEnd() {
+        // previousStation should become whatever's in nextStation
+        if(nextStation != null) {
+            prevStation = nextStation; 
+        } else if(prevStation != null) {
+            nextStation = prevStation; 
+        }
+    }
+
+    public String toString() {
+        return "ENDSTATION " + stop + ": " + color + " line, " + "in service: " + inService + 
+        ", previous station: " + (prevStation == null ? "none" : prevStation.stop) + ", next station: " + (nextStation == null ? "none" : nextStation.stop); 
     }
 }
